35f4253cc95f7cb454c0c14c1947a62ae30d9b7d
zettacamp-rahmatul-ramadhani <rahmatul.ramadhani@zettacamp.pro>
2022-10-13 09:51:05 +0700

    TS day 1

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-function combination(args1:any, args2:any, args3:any): string {
-    return "Typescript"
+const combination = (args1:string, args2:number, args3:number): string => {
+    return args1.slice(args2, args3)
 }
-console.log(combination("Learning Typescript is different than javascript", 9, 19));
+console.log(combination("Learning Typescript is different than Javascript", 9, 19));
+console.log(combination("Learning Typescript is different than javascript", 0, 12));
