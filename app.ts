0c82044f3784b37a9d9f7b1ed312576e3ea8f09f
zettacamp-rahmatul-ramadhani <rahmatul.ramadhani@zettacamp.pro>
2022-10-14 08:42:54 +0700

    TS day 2

---
+++ app.ts
@@ -0,0 +1,9 @@
+type argument = number | string
+
+function union(args: argument[]): string{
+    let result: string = args.join(" ")
+    return result
+}
+
+console.log(union([1, 'data', '3', 'result']));
+console.log(union(['Bejo', 'has', 4, 'car', 9, 'bycicle']));
