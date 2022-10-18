73626f4b77c56a25c46b04c632bfa6c75715c94c
zettacamp-rahmatul-ramadhani <rahmatul.ramadhani@zettacamp.pro>
2022-10-18 16:10:44 +0700

    Data Binding

--- app/src/app/main/article/article.component.ts
+++ app/src/app/main/article/article.component.ts
@@ -9,25 +9,28 @@ export class ArticleComponent implements OnInit{
 
   cardItems = [{
     title: "Title 1",
-    desc: "Desc 1"
+    desc: "Desc 1",
+    status: "Active"
   },{
     title: "Title 2",
-    desc: "Desc 2"
+    desc: "Desc 2",
+    status: "Active"
+  },{
+    title: "Title 3",
+    desc: "Desc 3",
+    status: "Danger"
   }]
 
-  text = "Active"
   style = "background-color: rgba(0, 255, 0, 1)"
   constructor() { }
 
   ngOnInit(): void {
   }
-  change() {
-    if (this.text === "Active") {
-      this.text = "Danger"
-      this.style = "background-color: rgba(255, 0, 0, 1)"
+  change(item: any) {
+    if (item.status === "Active") {
+      item.status = "Danger"
     } else {
-      this.text = "Active"
-      this.style = "background-color: rgba(0, 255, 0, 1)"
+      item.status = "Active"
     }
   }
 }
